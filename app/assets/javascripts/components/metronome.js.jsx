0bb37a5599839c29a4d8c3d46d47c2f46b75090a
Justin Schulz <justin.schulz@gmail.com>
2015-12-01 14:38:04 -0500

    .

---
+++ app/assets/javascripts/components/metronome.js.jsx
@@ -0,0 +1,9 @@
+window.Metronome = React.createClass({
+  render: function () {
+    return (
+      <div className="metronome-body">
+        <div className="metronome-outer-dial"></div>
+      </div>
+    )
+  }
+});
